e8140b13947a54a138ea00c1bfda44ccda09fa76
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-11-22 18:07:22 +0700

    Done

--- src/BTDatVeReDux/SeatRow.jsx
+++ src/BTDatVeReDux/SeatRow.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SeatRow extends Component {
@@ -11,61 +11,64 @@ class SeatRow extends Component {
         disabled = true;
       }
       let cssGheDangDat = "";
-      let indexGheDangDat = this.props.danhSachGheDangDat.findIndex(gheDangDat => gheDangDat.soGhe === ghe.soGhe)
+      let indexGheDangDat = this.props.danhSachGheDangDat.findIndex(
+        (gheDangDat) => gheDangDat.soGhe === ghe.soGhe
+      );
       if (indexGheDangDat !== -1) {
         cssGheDangDat = "gheDangChon";
       }
       return (
-        <button key={index} disabled={disabled} className={`ghe ${cssGheDaDat} ${cssGheDangDat}`} onClick={() => {
-          this.props.datGhe(ghe)
-        }}  >
+        <button
+          key={index}
+          disabled={disabled}
+          className={`ghe ${cssGheDaDat} ${cssGheDangDat}`}
+          onClick={() => {
+            this.props.datGhe(ghe);
+          }}>
           {ghe.soGhe}
         </button>
-      )
-    })
-  }
+      );
+    });
+  };
 
   renderSeatLayout = () => {
     if (this.props.soHangGhe === 0) {
-      return this.props.hangGhe.danhSachGhe.map((ghe,index) => {
+      return this.props.hangGhe.danhSachGhe.map((ghe, index) => {
         return (
-          <button key={index} className='rowNumber'>{ghe.soGhe}</button>
-        )
-      })
+          <button key={index} className="rowNumber">
+            {ghe.soGhe}
+          </button>
+        );
+      });
     } else {
       return (
         <>
           {this.props.hangGhe.hang} {this.renderGhe()}
         </>
-      )
+      );
     }
-  }
+  };
 
   render() {
-    return (
-      <>
-        {this.renderSeatLayout()}
-      </>
-    )
+    return <>{this.renderSeatLayout()}</>;
   }
 }
-
 
 const mapStateToProps = (state) => {
   return {
-    danhSachGheDangDat: state.ThongTinDatVeReducer.danhSachGheDangDat
-  }
-}
+    danhSachGheDangDat: state.ThongTinDatVeReducer.danhSachGheDangDat,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     datGhe: (ghe) => {
       dispatch({
         type: "BOOK_SEAT",
-        ghe
-      })
-    }
-  }
-}
+        ghe,
+      });
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SeatRow);
